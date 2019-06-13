8467a91479fadeb4df53eb9207f73e0c4501382c
Steve Persch <steve.persch@pantheon.io>
2019-06-13 12:01:19 -0700

    functioning query

--- github/index.js
+++ github/index.js
@@ -1,4 +1,5 @@
 const graphqlGot = require('graphql-got');
+const util = require('util');
 
 const query = `{
     organization(login: "nasa") {
@@ -7,8 +8,60 @@ const query = `{
     }
   }`;
 
-graphqlGot('https://api.github.com/graphql', {"query": query, "token": process.env.GITHUB_TOKEN}).then(response => {
-	console.log(response.body);
+
+const query2 = `
+  query {
+   
+    search(query: "stevector/stevector-composer", type: REPOSITORY, first: 100) {
+      edges {
+        node {
+         ... on Repository {
+           name,
+           url,
+           ... RepoStatus
+         }
+       }
+      }
+    }
+}
+fragment RepoStatus on Repository  {
+  name,
+  defaultBranchRef {
+    name,
+    target {
+      ... on Commit {
+        id,
+        messageBody,
+        author {
+          avatarUrl
+          date
+          email
+          name
+        },
+        ... CommitStatus
+      }
+    }
+  }
+}
+fragment CommitStatus on Commit {
+  status {
+    state,
+    contexts {
+      ... StatusContextInfo
+    }
+  }
+}
+fragment StatusContextInfo on StatusContext {
+  state,
+  context,
+  createdAt,
+  targetUrl
+}   
+  `
+  
+graphqlGot('https://api.github.com/graphql', {"query": query2, "token": process.env.GITHUB_TOKEN}).then(response => {
+   // console.log(response.body);
+    console.log(util.inspect(response.body, {showHidden: false, depth: null}))
 	/*
 	{
 		unicorn: {
