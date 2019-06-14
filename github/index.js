71737c0fe3ff839db93c1244696537aaf5ae13c8
Steve Persch <steve.persch@pantheon.io>
2019-06-14 12:37:18 -0500

    color codes

--- github/index.js
+++ github/index.js
@@ -1,18 +1,12 @@
 const graphqlGot = require('graphql-got');
-const util = require('util');
-
-
-
-
-
-
+// const util = require('util');
 
 const repos = [
     "stevector/nerdologues-d8",
     "stevector/stevector-composer",
     "stevector/migrate_pantheon",
     "pantheon-systems/terminus-build-tools-plugin",
-    "pantheon-systems/circleci-orbs",
+    "pantheon-systems/circleci-orb",
     "pantheon-systems/pantheon_advanced_page_cache",
     "pantheon-systems/pantheon-advanced-page-cache",
     "pantheon-systems/example-drops-8-composer",
@@ -22,7 +16,7 @@ const repos = [
   ];
 
   const search_string = repos.map(repoSlug => "repo:" + repoSlug).join(' ');
-  console.log(search_string);
+  //console.log(search_string);
 
 const query = `
 query DashboardQuery($searchstring: String!) {
@@ -51,11 +45,33 @@ fragment RepoStatus on Repository  {
 }
   `
   
+
 graphqlGot('https://api.github.com/graphql', {"query": query, variables: {"searchstring": search_string}, "token": process.env.GITHUB_TOKEN}).then(response => {
     //console.log(util.inspect(response.body.search.edges, {showHidden: false, depth: null}))
-    const simplified = {}
+    const simplified_repo_statuses = {}
+    const sorted_repo_statuses = {};
+    const sorted_color_codes = [];
     response.body.search.edges.forEach(function (edge) {
-        simplified[edge.node.nameWithOwner] = edge.node.defaultBranchRef.target.status.state;
-      });
-      console.table(simplified);
+      simplified_repo_statuses[edge.node.nameWithOwner] = edge.node.defaultBranchRef.target.status.state;
+    });
+    console.table(simplified_repo_statuses);
+
+    repos.forEach(function(repo) {
+      var status = simplified_repo_statuses[repo];
+      sorted_repo_statuses[repo] = status;
+      if (status === 'SUCCESS') {
+        var color_code = 'g';
+      }
+      else if (status === 'FAILURE') {
+        var color_code = 'r';
+      }
+      // white as fallback.
+      else {
+        var color_code = 'w';
+      }
+      sorted_color_codes.push(color_code);
+    });
+
+    console.table(sorted_repo_statuses);
+    console.table(sorted_color_codes);
 });
