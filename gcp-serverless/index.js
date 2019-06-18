4136f8ed569ed4019f16292b9e7e7ae9b1e472d5
Steve Persch <steve.persch@pantheon.io>
2019-06-17 21:24:43 -0500

    coding standards

--- gcp-serverless/index.js
+++ gcp-serverless/index.js
@@ -47,37 +47,37 @@ fragment RepoStatus on Repository  {
   `
 
 exports.http = (request, response) => {
-  graphqlGot('https://api.github.com/graphql', { 'query': query, variables: { 'searchstring': search_string }, 'token': process.env.GITHUB_TOKEN }).then(github_response => {
+  graphqlGot('https://api.github.com/graphql', { 'query': query, variables: { 'searchstring': search_string }, 'token': process.env.GITHUB_TOKEN }).then(githubResponse => {
     // console.log(util.inspect(response.body.search.edges, {showHidden: false, depth: null}))
-    const simplified_repo_statuses = {}
-    const sorted_repo_statuses = {}
-    const sorted_color_codes = []
-    github_response.body.search.edges.forEach(function (edge) {
-      simplified_repo_statuses[edge.node.nameWithOwner] = edge.node.defaultBranchRef.target.status.state
+    const simplifiedRepoStatuses = {}
+    const sortedRepoStatuses = {}
+    const sorted_colorCodes = []
+    githubResponse.body.search.edges.forEach(function (edge) {
+      simplifiedRepoStatuses[edge.node.nameWithOwner] = edge.node.defaultBranchRef.target.status.state
     })
-    // console.table(simplified_repo_statuses);
+    // console.table(simplifiedRepoStatuses);
 
     repos.forEach(function (repo) {
       // console.log(repo)
-      var status = simplified_repo_statuses[repo]
+      var status = simplifiedRepoStatuses[repo]
       // console.log(status)
-      sorted_repo_statuses[repo] = status
+      sortedRepoStatuses[repo] = status
       if (status === 'SUCCESS') {
-        var color_code = 'g'
+        var colorCode = 'g'
       } else if (status === 'FAILURE') {
-        var color_code = 'r'
+        var colorCode = 'r'
       }
       // white as fallback.
       else {
-        var color_code = 'w'
+        var colorCode = 'w'
       }
-      sorted_color_codes.push(color_code)
+      sorted_colorCodes.push(colorCode)
     })
 
-    const color_list = sorted_color_codes.join(',')
-    // conso le.table(sorted_repo_statuses);
-    // console.table(sorted_color_codes);
-    response.status(200).send(color_list)
+    const colorList = sorted_colorCodes.join(',')
+    // conso le.table(sortedRepoStatuses);
+    // console.table(sorted_colorCodes);
+    response.status(200).send(colorList)
   })
 }
 
