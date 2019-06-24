7e89c40b33ed02487d9e4745588b1bcb175a4868
Steve Persch <steve.persch@pantheon.io>
2019-06-23 21:19:29 -0500

    new file for color codes

---
+++ gcp-serverless/getColorCodes.js
@@ -0,0 +1,60 @@
+'use strict'
+
+// const { exec } = require('child_process');
+var exec = require('child_process').exec
+const config = require('./config.json')
+const graphqlGot = require('graphql-got')
+const query = require('./query.js')
+// const got = require('got')
+// const util = require('util');
+const repos = require('./repos.js')
+
+const searchString = repos.map(repoSlug => 'repo:' + repoSlug).join(' ')
+// console.log(searchString);
+
+
+
+exports.callparticle = (request, response) => {
+  graphqlGot('https://api.github.com/graphql', { 'query': query, variables: { 'searchstring': searchString }, 'token': config.GITHUB_TOKEN }).then(githubResponse => {
+    // console.log(util.inspect(response.body.search.edges, {showHidden: false, depth: null}))
+    const simplifiedRepoStatuses = {}
+    const sortedRepoStatuses = {}
+    const sortedColorCodes = []
+    githubResponse.body.search.edges.forEach(function (edge) {
+      simplifiedRepoStatuses[edge.node.nameWithOwner] = edge.node.defaultBranchRef.target.status.state
+    })
+    // console.table(simplifiedRepoStatuses);
+
+    repos.forEach(function (repo) {
+      // console.log(repo)
+      var status = simplifiedRepoStatuses[repo]
+      // console.log(status)
+      sortedRepoStatuses[repo] = status
+      var colorCode = 'w'
+      if (status === 'SUCCESS') {
+        colorCode = 'g'
+      } else if (status === 'FAILURE') {
+        colorCode = 'r'
+      }
+      sortedColorCodes.push(colorCode)
+    })
+
+    const colorList = sortedColorCodes.join(',')
+    // conso le.table(sortedRepoStatuses);
+    // console.table(sortedColorCodes);
+    // exec();
+
+    exec('curl https://api.particle.io/v1/devices/' + config.BALANCE1_ID + "/circleCi -d arg='" + colorList + "' -d access_token=" + config.PARTICLE_TOKEN)
+    // got.post("https://api.particle.io/v1/devices/" + config.BALANCE1_ID + "/circleCi",{
+    //   body: {
+    //     arg: colorList,
+    //     access_token: config.PARTICLE_TOKEN
+    //   }
+    // })
+    response.status(200).send(colorList)
+  })
+}
+
+exports.event = (event, callback) => {
+  callback()
+}
