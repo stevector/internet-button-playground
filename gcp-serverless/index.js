0a0e21459549e5c6daa6b70991578a9954db157e
Steve Persch <steve.persch@pantheon.io>
2019-06-23 23:02:12 -0500

    coding standards

--- gcp-serverless/index.js
+++ gcp-serverless/index.js
@@ -1,30 +1,26 @@
 'use strict'
-var exec = require('child_process').exec
 const config = require('./config.json')
 const getColorCodes = require('./getColorCodes')
 const got = require('got')
 
 const callParticle = async (colorList) => {
-  //const colorList = 'g,g,r,g,g,g,g,g,g,g,g'
-  //const colorList = await getColorCodes()
-  const particleURL = "https://api.particle.io/v1/devices/" + config.BALANCE1_ID + "/circleCi"
+  // const colorList = 'g,g,r,g,g,g,g,g,g,g,g'
+  // const colorList = await getColorCodes()
+  const particleURL = 'https://api.particle.io/v1/devices/' + config.BALANCE1_ID + '/circleCi'
 
-try {
-      const response = await got.post(particleURL,{
-          form: true,
-          body: {
-            "arg": colorList,
-            "access_token": config.PARTICLE_TOKEN
-          }
-        })
-        console.log(response.body);
-
-} catch (error) {
-  console.log(error.response.body)
+  try {
+    const response = await got.post(particleURL, {
+      form: true,
+      body: {
+        arg: colorList,
+        access_token: config.PARTICLE_TOKEN
+      }
+    })
+    console.log(response.body)
+  } catch (error) {
+    console.log(error.response.body)
+  }
 }
-
-}
-
 
 exports.callparticle = (request, response) => {
   const makeRequest = async () => {
