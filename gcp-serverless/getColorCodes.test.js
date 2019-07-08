69493143c76545ec2cbd24321d5c312fbdc90b4e
Steve Persch <steve.persch@pantheon.io>
2019-07-07 20:31:10 -0500

    adding jest test

---
+++ gcp-serverless/getColorCodes.test.js
@@ -0,0 +1,8 @@
+const getColorCodes = require('./getColorCodes')
+
+// async/await can be used.
+it('works with async/await', async () => {
+    expect.assertions(1);
+    const colorCodes = await getColorCodes()
+    expect(colorCodes).toBe("g,g,r,g,g,g,g,g,g,g,g,");
+  });
