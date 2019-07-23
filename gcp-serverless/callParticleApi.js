049e869f57019828dedd2bbd2a99091d86eefdb3
Steve Persch <steve.persch@pantheon.io>
2019-07-23 16:41:20 -0500

    code style

--- gcp-serverless/callParticleApi.js
+++ gcp-serverless/callParticleApi.js
@@ -2,8 +2,7 @@
 const got = require('got')
 
 module.exports = async (configObject) => {
-
-  const {particleToken, deviceId, colorList } = configObject
+  const { particleToken, deviceId, colorList } = configObject
   // const colorList = 'g,g,r,g,g,g,g,g,g,g,g'
   // const colorList = await getColorCodes()
   const particleURL = 'https://api.particle.io/v1/devices/' + deviceId + '/circleCi'
