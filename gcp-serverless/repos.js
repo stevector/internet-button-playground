a2db055eb2e9ab617840ba262555667810538e69
Open Source Al Gore <email@algore.website>
2019-08-02 13:04:38 -0500

    Update repos.js

--- gcp-serverless/repos.js
+++ gcp-serverless/repos.js
@@ -1,15 +1,15 @@
 'use strict'
 
 module.exports = [
+  'fauxalgore/BehatSayExtension',
+  'fauxalgore/algore.website',
+  'fauxalgore/boxlocks.com',
+  'fauxalgore/lockbox.rocks',
+  'fauxalgore/fauxalgore/TheRightBoxForBikeLocks.BoxLocks.com',
+  'fauxalgore/LockBlok.boxlocks.com',
+  'fauxalgore/orbs',
   'stevector/nerdologues-d8',
   'stevector/stevector-composer',
-  'stevector/migrate_pantheon',
-  'pantheon-systems/terminus-build-tools-plugin',
-  'pantheon-systems/circleci-orb',
-  'pantheon-systems/pantheon_advanced_page_cache',
-  'pantheon-systems/pantheon-advanced-page-cache',
-  'pantheon-systems/example-drops-8-composer',
-  'pantheon-systems/example-wordpress-composer',
-  'pantheon-systems/drops-8',
-  'pantheon-systems/WordPress'
+  'stevector/wordpress-orb-demo',
+  'stevector/pantheon-orbs',
 ]
